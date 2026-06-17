d9cb030cda28097e91348fedba64cfe8a3cbcdb2
talmado <tala.marcdonald@yahoo.de>
2026-06-17 11:10:06 +0200

    create project

---
+++ src/test/java/de/mtala/todomanagement/TodomanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mtala.todomanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodomanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
